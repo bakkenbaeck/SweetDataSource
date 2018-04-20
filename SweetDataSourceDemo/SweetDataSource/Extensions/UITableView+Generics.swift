d951ae75bbe93dd8b88718733271b2d4facdf3d0
Ellen Shapiro <designatednerd@gmail.com>
2018-04-20 15:31:17 +0200

    pull UITableViewCell stuff into its own file

--- SweetDataSourceDemo/SweetDataSource/Extensions/UITableView+Generics.swift
+++ SweetDataSourceDemo/SweetDataSource/Extensions/UITableView+Generics.swift
@@ -8,22 +8,6 @@
 
 import UIKit
 
-public extension UITableViewCell {
-
-    /// A default reuse identifier based on the cell's class.
-    public static var defaultReuseIdentifier: String {
-        return String(describing: type(of: self))
-    }
-
-    /// Gets a nib with a name matching that of the cell's class.
-    /// - Parameters:
-    ///    - bundle: The bundle to grab the nib from.
-    public static func defaultNib(inBundle bundle: Bundle = Bundle.main) -> UINib {
-        let nibName = String(describing: type(of: self))
-        return UINib(nibName: nibName, bundle: bundle)
-    }
-}
-
 public extension UITableView {
 
     public func registerCell<CellType: UITableViewCell>(of type: CellType.Type) {
---
+++ SweetDataSourceDemo/SweetDataSource/Extensions/UITableViewCell+DefaultValues.swift
@@ -0,0 +1,25 @@
+//
+//  UITableViewCell+DefaultValues.swift
+//  SweetDataSource
+//
+//  Created by Ellen Shapiro (Work) on 4/20/18.
+//  Copyright © 2018 Bakken & Bæck. All rights reserved.
+//
+
+import UIKit
+
+public extension UITableViewCell {
+
+    /// A default reuse identifier based on the cell's class.
+    public static var defaultReuseIdentifier: String {
+        return String(describing: type(of: self))
+    }
+
+    /// Gets a nib with a name matching that of the cell's class.
+    /// - Parameters:
+    ///    - bundle: The bundle to grab the nib from.
+    public static func defaultNib(inBundle bundle: Bundle = Bundle.main) -> UINib {
+        let nibName = String(describing: type(of: self))
+        return UINib(nibName: nibName, bundle: bundle)
+    }
+}
